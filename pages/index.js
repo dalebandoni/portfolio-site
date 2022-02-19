2c94b4b84496575fe537370cc5d735aa65e1d891
Dale Bandoni <dalebandoni.10@gmail.com>
2022-02-19 16:15:12 +0000

    set maintenance

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import Head from 'next/head'
-import Intro from '../sections/Intro'
-import Projects from '../sections/Projects'
-import JobProspect from '../sections/JobProspect'
-import About from '../sections/About'
-import Footer from '../sections/Footer'
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Dale Bandoni</title>
-        <link rel='icon' href='/assets/images/logoIcon.png' />
       </Head>
-      <Intro />
+      <div>This website is under maintenance.</div>
     </div>
   )
 }
